f25cbe53bda5c7ec55e7e5f104be8a63c3acec31
unknown <dl.kokhtashvili@gmail.com>
2024-06-07 20:28:04 +0400

    Add footer section for contact information

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,6 +4,12 @@ const HomePage = () => {
   return (
     <div>
       <ShoesForHomePage />
+      <footer>
+        <div className="">
+          <div className="h-[2px] w-full bg-gradient-to-r from-transparent via-slate-200 to-transparent"></div>
+        </div>
+        <div className="h-20 w-full">footer</div>
+      </footer>
     </div>
   );
 };
