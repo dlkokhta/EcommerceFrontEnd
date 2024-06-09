64fa4d40dd3e59f0f755c9c9169589a1bc92caad
unknown <dl.kokhtashvili@gmail.com>
2024-06-09 23:43:30 +0400

    add copyright notice to footer

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,19 +8,22 @@ const HomePage = () => {
   return (
     <div>
       <ShoesForHomePage />
-      <footer>
+      <footer className="font-light lg:px-10">
         <div>
           <div className="h-[2px] w-full bg-gradient-to-r from-transparent via-slate-200 to-transparent"></div>
         </div>
-        <div className="flex justify-between pb-10 pt-10">
+        <div className="flex items-center justify-between pb-10 pt-10">
+          <div className=" ">© 2024 DLK, Inc. All Rights Reserved</div>
           <div>
-            <div>home</div>
+            <div>Home:</div>
+            <div>Email:</div>
+            <div>Mobile:</div>
           </div>
           <div className="flex gap-3">
-            <img src={instagram} className="w-6" />
-            <img src={facebook} className="w-6" />
-            <img src={x} className="w-6" />
-            <img src={linkedin} className="w-6" />
+            <img src={instagram} className="h-6 w-6" />
+            <img src={facebook} className="h-6 w-6" />
+            <img src={x} className="h-6 w-6" />
+            <img src={linkedin} className="h-6 w-6" />
           </div>
         </div>
       </footer>
