d76fdf40e071dfd9a848414f05e38d0354b3be52
unknown <dl.kokhtashvili@gmail.com>
2024-06-11 23:05:54 +0400

    Add logo to the registration page

--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -4,6 +4,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import RegistrationSchema from "./RegistrationSchema.js";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 const Registration = () => {
   const {
@@ -39,6 +40,13 @@ const Registration = () => {
   return (
     <div>
       <div className=" px-16 pt-28 lg:px-60 xl:px-[470px] 2xl:px-[580px] 3xl:px-[750px] ">
+        <div className="flex justify-center">
+          <img
+            className="hidden w-16 cursor-pointer  lg:block"
+            src={logo}
+            onClick={() => navigate("/")}
+          />
+        </div>
         <form
           onSubmit={handleSubmit(onSubmit)}
           className="mb-10 flex flex-col gap-5 border border-slate-400 px-5 py-5"
